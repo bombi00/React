80859543be259ddee7f1b8ddd514ca070606da1b
Fbombi <filippo.bomben@studenti.unipd.it>
2023-12-01 15:36:57 +0100

    project1

--- my-app/src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return(
-        <footer className="footer">
-            <small>2023 Bomben development. All rights reserved</small>
-        </footer>
-    )
-}
